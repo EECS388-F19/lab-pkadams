29b085b73397a3ea8c24a2a5493117687196f4e3
Perry K Adamopoulos <pkadams@umich.edu>
2019-09-09 13:45:54 -0400

    added second example

---
+++ lab1-example.py
@@ -0,0 +1,12 @@
+students = ['Perry', 'Osamah', 'Ian']
+
+
+
+sorted_students = sorted(students)
+
+print(sorted_students)
+
+first_name = sorted_students[0]
+first_name = first_name[:-1]
+
+print(first_name)
