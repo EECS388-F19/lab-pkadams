a92dac9e39e035ed7f8b66d9f4dc8d7d6fc4fa83
Perry K Adamopoulos <pkadams@umich.edu>
2019-09-09 13:37:27 -0400

    finished

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+import random
+
+
+first_num = random.randint(0,101)
+second_num = random.randint(0,101)
+
+print(first_num)
+print(second_num)
+
+
+print('Sum = ' + str(first_num + second_num))
+print('Average = ' + str((first_num + second_num)/2))
